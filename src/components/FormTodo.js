9fbc71b95c5f24b769998b0ea6fac1410e48739d
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2022-03-17 09:31:37 +0100

    Todo List V1

---
+++ src/components/FormTodo.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+
+
+export default function TodoForm(props) {
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addTodo(input)
+        setInput("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="input-group mb-3">
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className="form-control"
+                placeholder="Titre de la tâche à ajouter !"
+                required={true}
+            />
+            <Button type={"submit"} variant="outline-primary">Ajouter</Button>{' '}
+
+        </form>
+    );
+}
+
---
+++ src/components/ItemTodo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+export default function TodoItem(props) {
+    const {todo, removeTodo, completeTodo} = props
+    return (
+        <div className={todo.completed ? "todo-row complete" : "todo-row"}>
+            {todo.text}
+            <div className="buttonsContainer">
+                <Button variant="danger" style={{marginRight: 5}} onClick={() => removeTodo(todo.id)}>Supprimer</Button>
+                <Button variant="success" onClick={() => completeTodo(todo.id)}>Fait</Button>{' '}
+            </div>
+        </div>
+    )
+}
+
